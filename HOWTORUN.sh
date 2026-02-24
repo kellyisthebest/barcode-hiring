00a6cfed3614e8842521d2f21e570088fc00b348
Kelly <115475106+kellyisthebest@users.noreply.github.com>
2026-02-24 13:32:51 +0000

    renamed prerequisites to HOWTORUN and made it more readable

--- HOWTORUN.sh
+++ HOWTORUN.sh
@@ -1,30 +1,38 @@
 ## Prerequisites
 
-# Check that docker is working 
+# 1 : Check that docker is working 
+# Power shell (PS) commands are prefixed with >>
+
 >> docker --version
 >> docker compose version
 
-# Copy .env.example to .env to ensure that the environment variables are set up correctly 
+# 2 : Copy .env.example to .env to ensure that the environment variables are set up correctly 
 # and DB will be on localhost:5432
+
 >> copy .env.example .env 
 
-# Staring Postgres with Docker compose
+# 3. Staring Postgres with Docker compose
 >> docker compose up -d
 # (to check if the container is running, you can use >> docker compose ps)
 
-# Run the migrations using the goose-docker image
+# 4. Run the migrations using the goose-docker image
 # note that the port is 5433 because we are forwarding the container's 5432 to our localhost's 5433
+# This project uses the kukymbr/goose-docker image.
+
 >> docker run --rm `
 >>  -v "${PWD}\db\migrations:/migrations" `
 >>  -e GOOSE_DRIVER="postgres" `
 >>  -e GOOSE_DBSTRING="host=host.docker.internal port=5433 user=postgres password=postgres dbname=barcode_db sslmode=disable" `
 >>  kukymbr/goose-docker:latest
 
-## Inspect the current table 
+____________________________________________________________________________________________________________
+
+## 5. Inspect database schema
 >> docker compose exec db psql -U postgres -d barcode_db -c "\d+ dirac.product"
 
-## How to run seed_products.sql to insert sample data into the database
+## 6a. How to run seed_products.sql to insert sample data into the database
 >> Get-Content .\seed_products.sql -Raw | docker compose exec -T db psql -U postgres -d barcode_db
 
-## How I piped this output to a file for the write-up
+## 6b. How I piped this output to a file for the write-up
+## Output for this demo is located in .\seed_run_output.txt
 >> Get-Content .\db\seed_products.sql -Raw |  docker compose exec -T db psql -U postgres -d barcode_db `  | Tee-Object -FilePath .\seed_run_output.txt
